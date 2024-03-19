0c38d7370f1fc259c43e1dd19453cec4b071c7be
elnobrianardi <elno.brianardi@gmail.com>
2024-03-19 23:51:42 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-[40px] first-letter:text-[50px] text-red-600'>hello react + vite app</h1> 
+    </>
+  )
+}
+
+export default App
